f85a5cd02e5df3f7440ebc9e60cef175745e77e6
Erick Davi Morgade Pessanha <erickdavi@gmail.com>
2021-02-12 19:13:31 -0300

    Update certexpire.sh
    Alteração na lógica do projeto

--- certexpire.sh
+++ certexpire.sh
@@ -3,13 +3,17 @@
 function countdown_cert(){
    if [ -z $2 ];then
       #Se o parametro porta vier vazio, considera a porta 443
-      EXPDATE=$(echo | openssl s_client -connect $1:443 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
+      PORT="443"
+      #EXPDATE=$(echo | openssl s_client -connect $1:443 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
 
    else
       #Insere no comando a porta indicada pelo usuario
-      EXPDATE=$(echo | openssl s_client -connect $1:$2 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
+      PORT=""
+      #EXPDATE=$(echo | openssl s_client -connect $1:$2 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
 
    fi
+
+EXPDATE=$(echo | openssl s_client -connect $1:$2 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
 DTEXP=$(date -d "$EXPDATE" +%j)
 echo "$DTEXP dias"
 
