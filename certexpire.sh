a338ad039fb0df9753308228e7d766b1380191a3
Erick Davi Morgade Pessanha <erickdavi@gmail.com>
2021-02-13 23:41:59 -0300

    Update certexpire.sh

--- certexpire.sh
+++ certexpire.sh
@@ -1,22 +1,67 @@
 #!/bin/bash
 
-function countdown_cert(){
-   if [ -z $2 ];then
-      #Se o parametro porta vier vazio, considera a porta 443
-      PORT="443"
-      #EXPDATE=$(echo | openssl s_client -connect $1:443 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
-
+function certexpire(){
+   if [ -z $HOST ];then
+      echo "O campo host nao pode vir vazio"
    else
-      #Insere no comando a porta indicada pelo usuario
-      PORT=""
-      #EXPDATE=$(echo | openssl s_client -connect $1:$2 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
+      if [ -z $PORTA ];then
+         #Se o parametro porta vier vazio, considera a porta 443
+         PORTA="443"
+      else
+         #Insere no comando a porta indicada pelo usuario
+         PORTA=$PORTA
+      fi
+
+      EXPDATE=$(echo | openssl s_client -connect $HOST:$PORTA -servername $HOST 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}' 2> /dev/null)
+      DTEXP=$(expr $(date -d "$EXPDATE" +%j) - $(date +%j))
+
+      if [ $DTEXP -lt 100 ]; then
+         DTEXP="$DTEXP dias(!)"
+      else
+         DTEXP="$DTEXP dias"
+      fi
+
+      echo "$HOST: $DTEXP"
 
    fi
 
-EXPDATE=$(echo | openssl s_client -connect $1:$2 -servername $1 2> /dev/null | openssl x509 -noout -dates | awk -F"=" '/notAfter/ {print $2}' | awk '{print $1" "$2}')
-DTEXP=$(date -d "$EXPDATE" +%j)
-echo "$DTEXP dias"
+}
+
+
+function check_by_file(){
+   if [ -z $FILE ];then
+      echo "O campo arquivo nao pode ser vazio"
+   else
+
+      while read LINE
+      do
+         HOST=$(echo $LINE | awk -F"," '{print $1}')
+         PORTA=$(echo $LINE | awk -F"," '{print $2}')
+         #echo $HOST
+
+         certexpire $HOST $PORTA
+
+      done < $FILE
+
+   fi
 
 }
 
-countdown_cert $1 $2
+function option(){
+   case $1 in
+      "-h")
+         HOST=$2
+         PORTA=$3
+         certexpire $HOST $PORTA
+      ;;
+      "-f")
+         FILE=$2
+         check_by_file $FILE
+      ;;
+      *)
+         echo "Opcao invalida"
+      ;;
+   esac
+
+}
+option $*
